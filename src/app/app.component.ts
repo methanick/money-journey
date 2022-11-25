ee5483ea6d3b55a7045288b27e1fe5973d5df7f4
methanick <metasit.b@tourwow.com>
2022-11-25 23:09:27 +0700

    intregrate prime ng framwork

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+items: MenuItem[]  = [
+  // {label: "Update", icon: 'pi pi-refresh', command: () => {
+  //     this.update();
+  // }},
+  // {label: 'Delete', icon: 'pi pi-times', command: () => {
+  //     this.delete();
+  // }},
+  {label: 'Angular.io', icon: 'pi pi-info', url: 'http://angular.io'},
+  {separator: true},
+  {label: 'Setup', icon: 'pi pi-cog', routerLink: ['/setup']}
+];
+
   title = 'my-app';
+
+  constructor(private primengConfig: PrimeNGConfig){}
+
+
+    ngOnInit() {
+      this.primengConfig.ripple = true;
+    }
+
+    // save(severity: string) {
+    //     this.messageService.add({severity:severity, summary:'Success', detail:'Data Saved'});
+    // }
+
+    // update() {
+    //     this.messageService.add({severity:'success', summary:'Success', detail:'Data Updated'});
+    // }
+
+    // delete() {
+    //     this.messageService.add({severity:'success', summary:'Success', detail:'Data Deleted'});
+    // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ButtonModule} from 'primeng/button';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {StyleClassModule} from 'primeng/styleclass'
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import {CheckboxModule} from 'primeng/checkbox';
+import { ChipModule } from 'primeng/chip';
+import {InputTextModule} from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    CommonModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    SplitButtonModule,
+    MessagesModule,
+    MessageModule,
+    RouterModule.forRoot([]),
+    StyleClassModule,
+    CheckboxModule,
+    ChipModule,
+    InputTextModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
